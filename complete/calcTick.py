487fddbafbbe393c8f70017a3fc2f638d0a91d9e
Teo Ivančević <teo@tomsoft.hr>
2023-04-01 22:10:54 +0200

    changes to main.py for production

--- complete/calcTick.py
+++ complete/calcTick.py
@@ -7,15 +7,6 @@ url = "http://192.168.1.101:3000"
 _api = ApiService(url)
 
 
-#user = "fermf"
-#secret = "1349d0f368babe13344db67d0c815bbb"
-#print("from api: " + str(_api.getPairs("BTC,USD")))
-
-#balances = _api.balance(user)
-#print("Balance: " + str(_api.balance(user)))
-
-#print("form api: " + str(_api.getAllBalances(secret)))
-
 def calcTickTime(estimatedTime, curPrec, curTick, prec = 0.05, timeDelta = 3.):
     if (curPrec < prec): return estimatedTime
     curTime = time.time()
