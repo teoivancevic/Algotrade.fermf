dda5474bcfd157b338216125930b2f6090e45716
dsabolic42 <domagojsabolic2003@gmail.com>
2023-04-02 00:28:21 +0200

    graph drugi nodeovi

--- graph.py
+++ graph.py
@@ -1,6 +1,7 @@
 import sys
+import random
 
-sys.path.append('teo test/')
+sys.path.append('teo_test/')
 
 from ApiService import ApiService
 api = ApiService('http://192.168.1.101:3000')
@@ -21,10 +22,26 @@ def find(i, j):
             return k
     return -1
 
+def get_path_vol(path):
+    vals = []
+    vals.append(vol_mat[node[path[0]]][node[path[1]]])
+    for i in range(1, len(path) - 1):
+        vals.append(min(vals[-1] * (e_mat[node[path[i]]][node[path[i + 1]]] / 1e8), vol_mat[node[path[i]]][node[path[i + 1]]]))
+
+    ret = vals[0]
+    div = 1
+    for i in range(1, len(vals)):
+        div *= e_mat[node[path[i]]][node[path[i + 1]]] / 1e8
+        #print('vals[' + str(i) + ']', vals[i])
+        ret = min(ret, vals[i] / div)
+        #print('div', div)
+    return int(ret)
+
 def get_min_vol(i, j, k, eij, ejk, eki):
     first = vol_mat[node[i]][node[j]] 
     second = min(first * eij, vol_mat[node[j]][node[k]])
     third = min(second * ejk, vol_mat[node[k]][node[i]])
+    print('get_min_vol', first, second, third, eki * ejk)
     return int(min(first, second / ejk, third / eki / ejk))
 
 
@@ -62,40 +79,59 @@ for key in graph:
     node.append(key)
 print(node.index('USDT'))
 
-for i in range(5, 6):
+def find_trig(i):
     for j in range(len(node)):
         for k in range(len(node)):
             if j == k:
                 continue
 
+
+
+def order_path(path):
+    trade_vol = min(int(get_path_vol(path) / (e_mat[node[path[0]]][node[path[1]]] / 1e8) * 9 / 10), int(int(api.balance(user['USDT']) * 9 /10)))
+    trade_vol = int(trade_vol)
+
+    print('vol: ', trade_vol / 1e8)
+    print('bal: ', api.balance(user)['USDT'] / 1e8, 'est_new_bal: ', trade_vol / 1e8 * eij * ejk * eki) 
+    url = ""
+    for i in range(len(path) - 1):
+        url += node[path[i]] + ',' + node[path[i + 1]] + str(trade_vol) + '|'
+        trade_vol = int(trade_vol * (e_mat[node[path[i]]][node[path[i + 1]]] / 1e8))
+
+    url = url[:-1]
+    print(url)
+    print(api.createOrders(user, secret, url))
+    print('bal: ', api.balance(user)['USDT'] / 1e8)
+    print('------')
+
+
+
+
+
+
             if node[j] in e_mat[node[i]] and node[k] in e_mat[node[j]] and node[i] in e_mat[node[k]]:
-                #eij = graph[node[i]][eij][1] / 1e8
-                #ejk = graph[node[j]][ejk][1] / 1e8
-                #eki = graph[node[k]][eki][1] / 1e8
                 eij = e_mat[node[i]][node[j]] / 1e8
                 ejk = e_mat[node[j]][node[k]] / 1e8
                 eki = e_mat[node[k]][node[i]] / 1e8
                 #print(eij* ejk* eki)
 
-                if eij * ejk * eki > 1.001 and get_min_vol(i, j, k, eij, ejk, eki) / 1e8 > 0.002:
+                #print('vol_paths: ', get_path_vol([i, j, k, i]) / 1e8, get_min_vol(i, j, k, eij, ejk, eki) / 1e8)
+                if eij * ejk * eki > 1.001 and get_path_vol([i, j, k, i]) / 1e8 > 0.002:
                     print("uso")
-                    #print('naso ciklus: ' + node[i] + ' ' + node[j] + ' ' + node[k] + ' omjer: ', eij * ejk * eki, 'vol: ', get_min_vol(i, j, k, eij, ejk, eki) / 1e8 )
 
                     #if get_min_vol(i, j, k, eij, ejk, eki) / 1e8 > 20:
                     print(node[i], '->', node[j], eij, vol_mat[node[i]][node[j]] / 1e8)
                     print(node[j], '->', node[k], ejk, vol_mat[node[j]][node[k]] / 1e8)
                     print(node[k], '->', node[i], eki, vol_mat[node[k]][node[i]] / 1e8)
 
-                    trade_vol = min(int(get_min_vol(i, j, k, eij, ejk, eki) / eij * 9 / 10), int(int(api.balance(user)['USDT']) * 9 / 10))
+                    trade_vol = min(int(get_path_vol([i, j, k, i]) / eij * 9 / 10), int(int(api.balance(user)['USDT']) * 9 / 10))
                     print('vol: ', trade_vol / 1e8)
 
                     trade_vol = int(trade_vol)
                     print('bal: ', api.balance(user)['USDT'] / 1e8, 'est_new_bal: ', trade_vol / 1e8 * eij * ejk * eki) 
-                    #print('trade_vol_nacpo[cetaku', trade_vol, vol_mat[node[i]][node[j]])
 
                     url = node[i] + ',' + node[j] + ',' + str(trade_vol)
                     trade_vol = int(trade_vol * eij)
-                    #print('trade_vol_drugitrade', trade_vol)
 
                     url = url + '|' + node[j] + ',' + node[k] + ',' + str(trade_vol)
                     trade_vol = int(trade_vol * ejk)
@@ -104,8 +140,27 @@ for i in range(5, 6):
                     print(api.createOrders(user, secret, url))
                     print('bal: ', api.balance(user)['USDT'] / 1e8)
                     print('------')
-                    #exit(0)
 
 
+
+def cmp(a):
+    return a[1]
+
+HOP_DIST=1
+CURR_NODE=5
+path = [5]
+for az in range(HOP_DIST):
+    vols = []
+    for neigh in vol_graph[node[CURR_NODE]]:
+        neigh_idx = node.index(neigh[0])
+        vols.append((neigh_idx, min(neigh[1] / e_mat[node[CURR_NODE]][node[neigh_idx]], vol_mat[node[neigh_idx]][node[CURR_NODE]])))
+
+    vols.sort(reverse=True,key=cmp)
+    next_node = min(len(vols), random.randint(0, 5))
+    path.append(next_node)
+    CURR_NODE = next_node
+
+
+find_trig(5)
 
 print(api.balance(user)['USDT'] / 1e8)
