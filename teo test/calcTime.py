3efe262b599b1933c77adf2b73a0c56794a02d1e
Petar Sruk <petar.sruk@gmail.com>
2023-04-01 13:20:07 +0200

    added bruteforce time finder

---
+++ teo test/calcTime.py
@@ -0,0 +1,42 @@
+import json
+from ApiService import ApiService
+from datetime import datetime
+import time
+
+url = "http://192.168.1.101:3000"
+_api = ApiService(url)
+
+
+user = "fermf"
+secret = "1349d0f368babe13344db67d0c815bbb"
+#print("from api: " + str(_api.getPairs("BTC,USD")))
+
+balances = _api.balance(user)
+#print("Balance: " + str(_api.balance(user)))
+
+#print("form api: " + str(_api.getAllBalances(secret)))
+
+def calcTickTime(estimatedTime, curPrec, prec = 0.05, timeDelta = 3.):
+    if (curPrec < prec): return estimatedTime
+    curTime = time.time()
+    time.sleep(estimatedTime - curPrec + timeDelta - curTime - 0.0001)
+    curTick = _api.getTime()
+    tickChanged = False
+    tick = curTick
+    tickdeltacalc = curPrec * 0.22
+    while True:
+        tick = _api.getTime()
+        if (tick != curTick):
+            return calcTickTime(time.time(), tickdeltacalc, prec, timeDelta)
+        time.sleep(tickdeltacalc)
+
+def bruteTime():
+    curTick = _api.getTime()
+    while True:
+        tick = _api.getTime()
+        if (tick != curTick):
+            return time.time()
+        time.sleep(0.22)
+
+
+print(calcTickTime(bruteTime(), 0.2, 0.005, 30))
