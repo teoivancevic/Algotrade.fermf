bc85159e34f963a2de841108f0c43e9c1c493f9d
Teo Ivančević <teo@tomsoft.hr>
2023-04-01 12:20:42 +0200

    Tick finder in main, kinda awful

--- teo test/main.py
+++ teo test/main.py
@@ -1,12 +1,44 @@
 import json
 from ApiService import ApiService
+from datetime import datetime
+import time
 
 url = "http://192.168.1.101:3000"
 _api = ApiService(url)
 
 
-
+user = "fermf"
 secret = "1349d0f368babe13344db67d0c815bbb"
 #print("from api: " + str(_api.getPairs("BTC,USD")))
 
-print("form api: " + str(_api.getAllBalances(secret)))
+balances = _api.balance(user)
+#print("Balance: " + str(_api.balance(user)))
+
+#print("form api: " + str(_api.getAllBalances(secret)))
+
+tickChanged = False
+countChanges = 0
+tick = _api.getTime()
+lastTick = tick
+
+timeChanged = datetime.now()
+lastTime = datetime.now()
+
+while countChanges < 5:
+    tick = _api.getTime()
+    if(tick != lastTick):
+        #tickChanged = True
+        timeChanged = datetime.now()
+        countChanges += 1
+        print(str(lastTime), str(timeChanged), str(tick))
+        #break
+    #else:
+        #print(str(datetime.now()) + ": " + str(tick))  
+
+    time.sleep(0.25)
+
+    lastTick = tick
+    lastTime = datetime.now()
+
+#print("Tick changed to " + str(tick) + " at " + str(timeChanged))
+#print(str(lastTime), str(timeChanged), str(tick))
