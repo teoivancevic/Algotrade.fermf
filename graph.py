95dddcf2e55d00d5f3e5b002ab635db0f1e2d17e
dsabolic42 <domagojsabolic2003@gmail.com>
2023-04-01 14:39:00 +0200

    asdf2

--- graph.py
+++ graph.py
@@ -3,6 +3,16 @@ api = ApiService('http://192.168.1.101:3000')
 
 data = api.getAllPairs()
 graph = {}
+node = []
+vol_graph = {}
+vol_mat = {}
+
+def find(i, j):
+    for k in range(len(graph[node[i]])):
+        if graph[node[i]][k][0] == node[j]:
+            return k
+    return -1
+
 
 for key in data:
     if key.startswith('close') == True:
@@ -13,22 +23,35 @@ for key in data:
             graph[node1] = []
         graph[node1].append((node2, data[key]));
 
+    if key.startswith('vol') == True:
+        node1 = key.split('_')[1].split(',')[0]
+        node2 = key.split('_')[1].split(',')[1]
 
-node = []
+        if not (node1 in vol_graph):
+            vol_graph[node1] = []
+        vol_graph[node1].append((node2, data[key]));
+
+
+for key in vol_graph:
+    vol_mat[key] = {}
+    for x in vol_graph[key]:
+        vol_mat[key][x[0]] = x[1]
+
 for key in graph:
     node.append(key)
+print(node.index('USDT'))
 
-for i in range(len(node)):
+for i in range(5, 6):
     for j in range(len(node)):
         for k in range(len(node)):
-            eij = graph[node[i]].find(j)
-            ejk = graph[node[j]].find(k)
-            eki = graph[node[k]].find(i)
+            eij = find(i, j)
+            ejk = find(j, k)
+            eki = find(k, i)
             if eij != -1 and ejk != -1 and eki != -1:
-                eij = graph[node[i]][eij] / 1e8
-                ejk = graph[node[j]][ejk] / 1e8
-                eki = graph[node[k]][eki] / 1e8
-                if eij * ejk * eki > 1:
-                    print('naso ciklus: ' + node[i] + ' ' + node[j] + ' ' + node[k])
+                eij = graph[node[i]][eij][1] / 1e8
+                ejk = graph[node[j]][ejk][1] / 1e8
+                eki = graph[node[k]][eki][1] / 1e8
+                if eij * ejk * eki > 1.001:
+                    print('naso ciklus: ' + node[i] + ' ' + node[j] + ' ' + node[k] + ' omjer: ', eij * ejk * eki, 'vol: ', min(vol_graph[node[i]][node[j]], vol_graph[node[j]][node[k]], vol_graph[node[k]][node[i]]))
 
 
